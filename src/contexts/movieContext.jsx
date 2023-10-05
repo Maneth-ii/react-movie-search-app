2202c97cbe375ca7daa9a640598086693033b1c0
Maneth Thimasha <125687840+Maneth-ii@users.noreply.github.com>
2023-10-05 21:40:58 +0530

    Update movieContext.jsx

--- src/contexts/movieContext.jsx
+++ src/contexts/movieContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState, useContext } from "react";
 import axios from "axios";
 
 // Replace 'YOUR_API_KEY' with your actual API key
-const API_KEY = '6cc99fc0';
+const API_KEY = import.meta.env.OMDB_APP_API_KEY;
 
 const MovieContext = createContext();
 
